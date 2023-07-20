71ab3eb917326f3560e5a24b8fb79964024ec924
Sourav_Gouda <gouda.sourav75@gmail.com>
2023-07-21 01:38:22 +0530

    cut out the garbage

--- app/page.js
+++ app/page.js
@@ -12,10 +12,8 @@ const LandingPage = () => {
       <main className="container mx-auto mt-10">
         <section className="text-center">
           <h1 className="text-4xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text bg-300% animate-gradient ">Welcome to Our Website</h1>
-          {/* <h1 className="text-4xl font-semibold mb-4 text-gradient text-center">Welcome to Our Website</h1> */}
           <p className="pt-10 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod ex ut justo volutpat elementum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
          <Link href="/register" className="mt-6 inline-block px-6 py-3 rounded bg-gradient-to-r from-blue-600 via-green-500 bg-300% animate-gradient">Learn More</Link>
-          {/* <a href="#" className="mt-6 inline-block px-6 py-3 rounded bg-blue-600 text-white font-semibold hover:bg-blue-700">Learn More</a> */}
         </section>
 
         <section className="mt-16">
